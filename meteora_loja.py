3a510df959c0bacb332e5588d94d506ae4fb5959
Larissa Irene Aguiar <83132717+larissa92@users.noreply.github.com>
2024-04-11 09:20:22 -0300

    Update meteora_loja.py
    imprimir resultado com base na descrição do produto

--- meteora_loja.py
+++ meteora_loja.py
@@ -1,1 +1,15 @@
 
+// imagine que você está desenvolvendo em Python para a Meteora, uma loja de roupas e-commerce. 
+
+Você está no processo de adicionar descrições de produtos ao site e precisa usar a função print em python para exibir as descrições na página.
+As descrições dos produtos incluem várias linhas e parágrafos.
+
+Quais das seguintes opções você escolheria para imprimir corretamente uma descrição de produto de várias linhas usando a função print em Python? //
+
+
+print("Camiseta Unissex","Tamanho: P, M, G, GG","Material: 100% algodão","Cores disponíveis: Preto, Branco, Vermelho", sep ='\n')
+
+print('''Camiseta Unissex
+Tamanho: P, M, G, GG
+Material: 100% algodão
+Cores disponíveis: Preto, Branco, Vermelho''')
