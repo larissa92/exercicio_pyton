4b10b5d238a50b2ce447331f8e25c173b9d727da
Larissa Irene Aguiar <83132717+larissa92@users.noreply.github.com>
2024-04-11 09:06:16 -0300

    Create tela.py
     1. Exercicio

---
+++ tela.py
@@ -0,0 +1,8 @@
+
+// exercicios do curso de Pyton : crie sua primeira aplicação | Alura Cursos  11/04/2024 //
+
+//1 - Imprima a frase: Python na Escola de Programação da Alura.//
+
+print('Python na Escola de Programação da Alura')
+
+  
